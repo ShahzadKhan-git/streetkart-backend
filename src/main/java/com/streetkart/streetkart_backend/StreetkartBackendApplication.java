baec722c587605ad50c8d7c2abc8e67664489cb0
Shahzad Khan <shahzad14103@gmail.com>
2026-02-15 15:03:58 +0530

    chore: initial Spring Boot setup with PostgreSQL and test controller

---
+++ src/main/java/com/streetkart/streetkart_backend/StreetkartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.streetkart.streetkart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetkartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetkartBackendApplication.class, args);
+	}
+
+}

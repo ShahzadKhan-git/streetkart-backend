7ab318a3d61c1003783a382bcd7315fba7ee5e54
Shahzad Khan <shahzad14103@gmail.com>
2026-03-08 16:19:45 +0530

    feat: implement JWT login authentication

--- src/main/java/com/streetkart/streetkart_backend/controller/AuthController.java
+++ src/main/java/com/streetkart/streetkart_backend/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.streetkart.streetkart_backend.controller;
 
+import com.streetkart.streetkart_backend.dto.LoginRequest;
 import com.streetkart.streetkart_backend.dto.RegisterRequest;
 import com.streetkart.streetkart_backend.entity.Role;
 import com.streetkart.streetkart_backend.entity.User;
 import com.streetkart.streetkart_backend.repository.UserRepository;
+import com.streetkart.streetkart_backend.security.JwtUtil;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -39,5 +42,25 @@ public class AuthController {
         userRepository.save(user);
 
         return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+
+        var userOpt = userRepository.findByEmail(request.getEmail());
+
+        if (userOpt.isEmpty()) {
+            return ResponseEntity.badRequest().body("Invalid email");
+        }
+
+        User user = userOpt.get();
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            return ResponseEntity.badRequest().body("Invalid password");
+        }
+
+        String token = JwtUtil.generateToken(user.getEmail());
+
+        return ResponseEntity.ok(token);
     }
 }
